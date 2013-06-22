3775f7da692ab41c8defee9c4d13dcee0c8e7159
Nicola Fontana <ntd@entidi.it>
2013-06-22 23:22:36 +0200

    Improved test cases

--- tests/TocifierTest.php
+++ tests/TocifierTest.php
@@ -4,23 +4,36 @@ require_once '../code/Tocifier.php';
 
 class TocifierTest extends PHPUnit_Framework_TestCase {
 
-    public function testInput() {
+    public function testProcess() {
         $tocifier = new Tocifier(1234);
         $this->assertFalse($tocifier->process());
 
         $tocifier = new Tocifier('');
         $this->assertFalse($tocifier->process());
+
+        $tocifier = new Tocifier(null);
+        $this->assertFalse($tocifier->process());
+
+        $tocifier = new Tocifier(array('1234'));
+        $this->assertFalse($tocifier->process());
+
+        $tocifier = new Tocifier('1234');
+        $this->assertTrue($tocifier->process());
     }
 
-    public function testParser() {
+    public function testHtml() {
         $tocifier = new Tocifier(@file_get_contents('test1'));
+        $this->assertEquals($tocifier->getHtml(), '');
         $this->assertTrue($tocifier->process());
+        $this->assertStringEqualsFile('html1', $tocifier->getHtml());
+    }
 
-        // Check the augmented HTML
-        $returned = $tocifier->getHtml();
-        $this->assertStringEqualsFile('html1', $returned);
+    public function testTOC() {
+        $tocifier = new Tocifier(@file_get_contents('test1'));
+        $this->assertEquals($tocifier->getTOC(), array());
+        $this->assertTrue($tocifier->process());
+        $this->assertNotNull($tocifier->getTOC());
 
-        // Check the TOC
         ob_start();
         $tocifier->dumpTOC();
         $returned = ob_get_clean();
