72bee4a1f54e3fb52f8cfc2b32f360267fa70038
Nicola Fontana <ntd@entidi.it>
2017-12-29 19:44:32 +0100

    Fix Hacks docblocks

--- src/Hacks.php
+++ src/Hacks.php
@@ -12,7 +12,7 @@ class Hacks extends ViewableData
      * Similar to addWrapperMethod() but made public and working on
      * custom instances to allow to inject custom wrappers.
      *
-     * @param string $instance
+     * @param ViewableData $instance
      * @param string $method
      * @param string $wrap
      */
@@ -27,6 +27,12 @@ class Hacks extends ViewableData
     }
 
     /**
+     * Add callback as a method.
+     *
+     * Similar to addCallbackMethod() but made public and working on
+     * custom instances to allow to inject custom callbacks.
+     *
+     * @param ViewableData $instance
      * @param string $method
      * @param \Closure $callback
      */
